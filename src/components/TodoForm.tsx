130af26da5ed6c0cbe0a21913334e8b6516a5add
recondito <santanallanes@hotmail.com>
2024-10-26 16:37:00 -0400

    Added option to Create tasks

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,17 +1,42 @@
-import { useForm } from "react-hook-form";
+import { useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+interface FormValues {
+  name: string;
+}
+
+interface Task {
+  name: string;
+}
 
 const TodoForm : React.FC = () => {
 
-    const { register, handleSubmit } = useForm();
-    const onSubmit : any = (data : any) => console.log(data);
+    const { register, handleSubmit, reset } = useForm<FormValues>();
+    const [Tasks, setTasks] = useState<Task[]>([])
+    const onSubmit : SubmitHandler<FormValues> = (data : any) => {
+      setTasks([...Tasks, {name: data.name}]);
+      reset();
+    }
+
+    console.log(Tasks);
 
   return (
+
     <>
         <form onSubmit={handleSubmit(onSubmit)}>
-            <input {...register("task")} />
+
+            <input 
+              {...register("name", {
+                required: true,
+              })} 
+              placeholder="Task Name"
+            />
+
             <input type="submit" />
+
         </form>
     </>
+
   )
 }
 
