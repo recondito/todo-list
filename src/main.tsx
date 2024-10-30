53af8196ffd10b6d444056e4749428bdcdd259fb
recondito <santanallanes@hotmail.com>
2024-10-30 12:34:34 -0400

    Wrapped main.tsx in TaskProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { TaskProvider } from './context/TaskContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <TaskProvider>
+      <App />
+    </TaskProvider>
   </StrictMode>,
 )
